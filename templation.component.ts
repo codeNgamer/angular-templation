e4d9252c5c1cc3e6d694c91f74be393f1dcf01e6
codeNgamer <ekeneoduah@live.com>
2016-12-01 19:04:37 -0600

    version bump and di enhancements

--- templation.component.ts
+++ templation.component.ts
@@ -3,6 +3,7 @@ import {
   NgModule,
   ComponentFactoryResolver,
   Input,
+  Injector,
   ComponentRef,
   ViewContainerRef,
   Compiler,
@@ -33,6 +34,7 @@ export class TemplationComponent {
     if (!this.component || !hasTemplate) return false;
     const templatedComponent = this.returnTemplatedComponent();
     const templationModule = this.returnTemplationModule({ templatedComponent });
+
     this.compiler.compileModuleAndAllComponentsAsync(templationModule)
     .then(factory => {
       const compFactory = factory.componentFactories.find(x => x.componentType === templatedComponent);
@@ -40,12 +42,33 @@ export class TemplationComponent {
     })
   }
 
+  private getDeps(func) {
+    // First match everything inside the function argument parens.
+    const args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
+    // Split the arguments string into an array comma delimited.
+    return args.split(',').map(function(arg) {
+     // Ensure no inline comments are parsed and trim the whitespace.
+      return arg.replace(/\/\*.*\*\//, '').trim();
+    }).filter(function(arg) {
+     // Ensure no undefined values are added.
+      return arg;
+    });
+  }
+
   returnTemplatedComponent() {
-    const componentMeta = { selector: 'templatedComponent' };
+    const componentDeps = this.getDeps(this.component.constructor);
+    const diParams = componentDeps.map(dep => this.component[dep]);
+
+    const componentMeta = { 
+      selector: 'templatedComponent',
+    };
     const templateType = this.templatePath ? 'templateUrl' : 'template';
     componentMeta[templateType] = (templateType === 'templateUrl') ? this.templatePath : this.template;
     @Component(componentMeta)
-    class TemplatedComponent extends this.component {}
+    class TemplatedComponent extends this.component {
+      constructor() {
+      }
+    }
     return TemplatedComponent;
   }
 
